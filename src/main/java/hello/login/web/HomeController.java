bde6cb6d7c242078de474700cdc2974c40200aa4
lazzzykim <33sunghyeon@gmail.com>
2025-09-16 18:36:39 +0900

    로그인 - 세션 직접 개발

--- src/main/java/hello/login/web/HomeController.java
+++ src/main/java/hello/login/web/HomeController.java
@@ -1,7 +1,8 @@
-package hello.login.web.session;
+package hello.login.web;
 
 import hello.login.domain.member.Member;
 import hello.login.domain.member.MemberRepository;
+import hello.login.web.session.SessionManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
